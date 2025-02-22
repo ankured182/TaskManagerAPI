ad13a99948329400ed11e1d6ed261744a4d0fa04
ankur debnath <ankured182@gmail.com>
2025-02-23 01:52:54 +0600

    Fixed dependency issue

--- obj/Debug/net8.0/TaskManagerAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskManagerAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5d5b454be75796dfe3d35721561515606115a8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0f1082536ca0d35bc9e49e1e86fd7237489069d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
